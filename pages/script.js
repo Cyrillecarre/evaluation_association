4c56ec80950b8f57f75e8ee5baa2e8f9d80ee830
Cyrille CARRE <cyrille.carre@gmail.com>
2023-08-16 15:17:43 +0200

    creation des liens entre page

---
+++ pages/script.js
@@ -0,0 +1,21 @@
+        var contactButton = document.getElementById("contacts");
+        contactButton.addEventListener("click", function() {
+            window.location.href = "/pages/page1/index4.html";
+        });
+
+        var evenementsButton = document.getElementById("evenements");
+        evenementsButton.addEventListener("click", function() {
+            window.location.href = "/pages/page1/index2.html";
+        });
+
+        var albumButton = document.getElementById("album");
+        albumButton.addEventListener("click", function() {
+            window.location.href = "/pages/page1/index3.html";
+        });
+
+        var accueilButton = document.getElementById("accueil");
+        accueilButton.addEventListener("click", function() {
+            window.location.href = "/pages/page1/index.html";
+        });
+
+        
